f1717a3e33b351b5e2dfb496f4c5e00b84ca925f
mab-mac <m.s.ramzy@gmail.com>
2016-08-15 01:20:41 +0200

    feature: implemented the retrieval of apps

--- app/src/main/java/net/minasamy/reactiveprogrammingdemo/AppListActivity.java
+++ app/src/main/java/net/minasamy/reactiveprogrammingdemo/AppListActivity.java
@@ -1,8 +1,14 @@
 package net.minasamy.reactiveprogrammingdemo;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import net.minasamy.reactiveprogrammingdemo.model.AppInfo;
+import net.minasamy.reactiveprogrammingdemo.util.Utils;
+
+import rx.Observable;
+import rx.Observer;
 
 public class AppListActivity extends AppCompatActivity {
 
@@ -11,6 +17,25 @@ public class AppListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_list);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //load apps list
+        Observable.from(Utils.getAppsList(this))
+                .subscribe(new Observer<AppInfo>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(AppInfo appInfo) {
+                        Log.i("App",appInfo.getLabel());
+                    }
+                });
     }
 
 }
---
+++ app/src/main/java/net/minasamy/reactiveprogrammingdemo/util/Utils.java
@@ -0,0 +1,33 @@
+package net.minasamy.reactiveprogrammingdemo.util;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
+
+import net.minasamy.reactiveprogrammingdemo.model.AppInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by mab on 8/15/16.
+ */
+public class Utils {
+
+    public static List<AppInfo> getAppsList(Context context) {
+        List<AppInfo> apps = new ArrayList<>();
+        final Intent appsIntent = new Intent(Intent.ACTION_MAIN);
+        appsIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        List<ResolveInfo> resolveInfos = context.getPackageManager()
+                .queryIntentActivities(appsIntent, PackageManager.COMPONENT_ENABLED_STATE_DEFAULT);
+        for(ResolveInfo info:resolveInfos){
+            Drawable logo=info.loadIcon(context.getPackageManager());
+            String label=info.loadLabel(context.getPackageManager()).toString();
+
+            apps.add(new AppInfo(logo,label));
+        }
+        return apps;
+    }
+}
